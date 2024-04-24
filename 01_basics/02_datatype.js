563605eda3a9b3aad11b2f435c060c72b6a9fae3
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-24 10:01:49 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+let name = "paras"
+let age = 20
+let isLoggedin = false
+
+// number => 2 to power 23
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefine
