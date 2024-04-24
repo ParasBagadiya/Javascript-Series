43b4f36ffee5a15b80d52ffaa9ca3af3b4be71da
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-24 10:35:22 +0000

    conversion of number

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "paras"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "paras"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "paras" => true
+
+let someNumber =22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
