d4a87a394d876871727c4d77c79383e6c75e6aa4
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-24 07:35:39 +0000

    variable

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,8 @@
+const accounID = 12
+let Email = "p@gmail.com"
+var Password = "123"
+city = "surat"
+
+// accountId = 2 // not allowed because we can not change const variable
+
+console.log(accounID);
