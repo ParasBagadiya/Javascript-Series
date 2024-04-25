6f4d04c2e29d8ca17c653627b1f0d76efaa3087e
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-25 15:28:07 +0000

    datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,29 +1,45 @@
-// primitive
+// // primitive
 
-// 7 type: String, Number, null, Boolearn, undefined, Symbol, BigInt
+// // 7 type: String, Number, null, Boolearn, undefined, Symbol, BigInt
 
-const score = 100;
-const scoreValue = 100.3
+// const score = 100;
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-// reference (non primitive)
+// // reference (non primitive)
 
-// array, Objects, Function
+// // array, Objects, Function
 
-const heros = ["shaktiman", "naagraj", "minal"];
-let myObj = {
-    name: "paras",
-    age: 22,
+// const heros = ["shaktiman", "naagraj", "minal"];
+// let myObj = {
+//     name: "paras",
+//     age: 22,
+// }
+
+// const myFunction  = function(){
+//     console.log("hello")
+// }
+
+// console.log(typeof outsideTemp)
+
+let myName = "paras"
+
+let surName = myName
+surName = "bagadiya"
+
+console.log(myName)
+console.log(surName)
+
+let userOne = {
+    email: "p@gmail.com",
+    upi: "L@lic"
 }
 
-const myFunction  = function(){
-    console.log("hello")
-}
-
-console.log(typeof outsideTemp)
+let userTwo = userOne
+userTwo.email = "paras  "
