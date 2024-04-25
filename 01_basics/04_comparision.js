ea24382d7f27fbf0afb2ccae0677d9bc3b08595d
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-25 15:03:56 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -9,5 +9,7 @@ console.log(null == 0);
 console.log(null >= 0);
 
 console.log(undefined == 0)
-console.log(undefined < 0)
 console.log(undefined > 0)
+console.log(undefined < 0)
+
+console.log("2" === 2)
