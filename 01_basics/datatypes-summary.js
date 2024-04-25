d871bbfcfb77ec18836c168351a973b247938fcd
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-04-25 15:03:35 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 type: String, Number, null, Boolearn, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// reference (non primitive)
+
+// array, Objects, Function
+
+const heros = ["shaktiman", "naagraj", "minal"];
+let myObj = {
+    name: "paras",
+    age: 22,
+}
+
+const myFunction  = function(){
+    console.log("hello")
+}
+
+console.log(typeof outsideTemp)
