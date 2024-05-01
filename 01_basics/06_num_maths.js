8e6a0cd8e923c1e521f383bbfe0a3d335bba712c
Paras <159463271+ParasBagadiya@users.noreply.github.com>
2024-05-01 07:09:52 +0000

    math and num

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+const otherNumeber = 1123.99
+
+// console.log(otherNumeber.toPrecision(2))
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++  math  +++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-3))
+// console.log(Math.round(6.9))
+// console.log(Math.ceil(3.2))
+// console.log(Math.floor(5.9))
+// console.log(Math.min(2, 4, 6, 9))
+// console.log(Math.max(2, 4, 6, 9))
+
+console.log(Math.random())
+console.log((Math.random()*10) +1);
+console.log((Math.floor(Math.random())*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
